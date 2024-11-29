4d9c39e1d61964956de37f0b29348ce0e55c4ca1
bouzidi khaled <bwzydy81@gmail.com>
2024-11-29 15:30:58 -0800

    my first time

---
+++ script.js
@@ -0,0 +1,46 @@
+const khaled = (a, b) => {
+    return a*b
+}
+const rusalt = (khaled (5, 5));
+console.log(rusalt);
+
+
+
+const bouzidi = (h, k) => {
+    return h-k
+}
+const rusalo = (bouzidi(6, 5));
+console.log(rusalo);
+
+const abdou = (o, l) =>{
+    return o/l
+}
+const raauslt = (abdou(5, 5));
+console.log(raauslt);
+
+// khaled
+const long = ["العربية","الفرنسية"]
+console.log(long);
+long.push("الايطالية");
+long.pop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
